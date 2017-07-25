e590238c9307457b613ab2facb8f4020f9620da7
Com03 <com03@Com03s-iMac.local>
2017-07-25 14:11:39 +0700

    Operated

---
+++ Operated.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var strNumber: String = "5"
+
+var intNumber: Int = 20
+
+let intNewNumber = Int(strNumber) //Change Strint ==> Int
+
+
+
+
+
+let intAnswer = intNewNumber! + intNumber
+let intAnswer2 = intAnswer/intNewNumber!
