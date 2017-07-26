94f67ab8d9385db31925eb4a79b19d4836af177a
Com03 <com03@com03s-imac.local>
2017-07-26 15:18:03 +0700

    if else

---
+++ ifElseStatament.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let intNumber: Int  = 70
+
+if intNumber > 50 {
+    print("คุณสอบผ่าน คะแนนของคุณคือ\(intNumber)")
+}   else{
+    print("คุณสอบไม่ผ่าน คะแนนของคุณแค่\(intNumber)")
+}
+		
