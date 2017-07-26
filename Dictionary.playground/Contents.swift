3547e0bc86e61bb404d69323635b3a312b2eb203
Com03 <com03@com03s-imac.local>
2017-07-26 14:35:40 +0700

    add code

---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playgrounjd - noun: a place where people can play
+
+import UIKit
+
+
+//Assige Value For Dictionary Type
+var strDicName = ["android":"ระบบปฎิบัติการแอนดรอนต์", "iOS":"ระบบปฎิบัติการบน iPhone"]
+
+
+//How to use Dictionary
+print("android คือ  \(strDicName["android"]!)")
+print("iOS คือ  \(strDicName["iOS"]!)")
+
+
+//Add New Member to Dictionary Type
+print("member of DIctionary ==> \(strDicName.count)")
+
+strDicName["Windows"] = "ระบบปฎิบัติการ Windows"
+
+print("member of Dictionary ==> \(strDicName.count)")
+print("strDicName ล่าสุด ==> \(strDicName["Windows"]!)")
+
+// Remove Member of Dictionary
+print(strDicName)
+
+strDicName.removeValue(forKey: "iOS")
+print(strDicName)
+
--- Operated.playground/Contents.swift
+++ Operated.playground/Contents.swift
@@ -7,11 +7,39 @@ var strNumber: String = "5"
 
 var intNumber: Int = 20
 
-let intNewNumber = Int(strNumber) //Change Strint ==> Int
+//let intNewNumber = Int(strNumber) //Change Strint ==> Int
 
 
 
 
 
-let intAnswer = intNewNumber! + intNumber
-let intAnswer2 = intAnswer/intNewNumber!
+//let intAnswer = intNewNumber! + intNumber
+//let intAnswer2 = intAnswer/intNewNumber!
+
+let strName = "Chaiwoot"
+let strSurname = "Phrombutr"
+let strSpace = " "
+
+let strGeneral = strName + strSpace + strSurname
+print(strGeneral)
+
+
+let douNumber1 = 12.34
+let douNumber2 = 10.5
+let douAnswer = douNumber1 + douNumber2
+
+print("คำตอบของ \(douNumber1) + \(douNumber2)  มีค่าเท่ากับ \(douAnswer)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
