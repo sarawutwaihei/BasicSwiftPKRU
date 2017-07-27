0f3aa2efbbb498548fa0772eea8941964d68a65f
Com03 <com03@Com03s-iMac.local>
2017-07-27 15:01:37 +0700

    Project Succes

--- ClassAndMethod.playground/Contents.swift
+++ ClassAndMethod.playground/Contents.swift
@@ -8,7 +8,36 @@ class MyClass {
     var intNumber: Int = 10
     var strName: String = "Doramon"
     var botStatus: Bool  = true
+    
+    //Create Method Type Void
+    
+    func myMethodFirst() -> Void {
+        print("my MehodFirst Valus of Name ==> \(strName)")
+    }
+    
+    //Create Method Type Return
+    func myAddTen() -> Int {
+        intNumber =  intNumber + 10
+        return intNumber
+    }
+    
+    //Create Method Type Argument & Return
+    func calculateArea(intBase: Int, intHeigh: Int) -> Double {
+        
+        
+        let douBase  = Double(intBase)
+        let douHeigh = Double(intHeigh)
+        let douArea  = 0.5 * douBase * douHeigh
+        
+        return douArea
+        
+        
+        
+        
+    }
 
+    
+    
     }// My class
 
 
@@ -27,3 +56,37 @@ print("Status ==> \(myClass.botStatus)")
 ////การ Setter คือการกำหนดค่า ให้กับ Variable ของ คลาสอื่น
 myClass.strName = "Nopita"
 print("Name ต่อมา ==> \(myClass.strName)")
+myClass.intNumber = 200
+print("Number ต่อมา ==>\(myClass.intNumber)")
+
+
+
+//การเรียกใช้ เมธอดแบบ Void
+myClass.myMethodFirst()
+
+//การเรียนใช้ เมธอด แบบ return
+var intResult = myClass.myAddTen()
+print("ค่าของ intResult ==> \(intResult)")
+
+
+
+intResult = myClass.myAddTen()
+print("ค่าใหม่ ของ inResult ==> \(intResult)")
+
+//การเรียกใช้ Method Type Argment and Return
+let myArea = myClass.calculateArea(intBase: 9, intHeigh: 20)
+print("myArea ==> \(myArea)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
